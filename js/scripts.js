8fa9002a17820cbe5b5bc26c8484c9af4af1bf7f
Hillary Hintzen <hillary07@gmail.com>
2016-01-19 15:56:58 -0800

    create initial files and write first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var triangle = function(side1, side2, side3) {
-  if (side1+side2<side3 || side2+side3<side1 || side3+side1<side2 ) {
+  if (side1+side2<side3 || side2+side3<side1 || side3+side1<side2) {
     return "not a triangle"
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,10 @@
 describe("triangle", function (){
-  it("is not a triangle if the sum of the lengths of any two sides is <= the third side", function(){
+  it("is not a triangle if the sum of two sides are <= the third side", function(){
     expect(triangle(4,4,10).to.equal("not a triangle"));
+  });
+
+  it("is a triangle if all sides are equal", function(){
+    expect(triangle(13,13,13).to.equal("equilateral triangle"));
   });
 
 });
