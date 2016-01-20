04e474923e1bf402e77e73560d194f87794336da
Hillary Hintzen <hillary07@gmail.com>
2016-01-19 17:07:11 -0800

    update scripts and triangle files to make specs work in active site

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 var triangle = function(side1, side2, side3) {
   if (side1+side2<side3 || side2+side3<side1 || side3+side1<side2) {
-    return "not a triangle"
-  }
-
-  if (side1 === side2 && side2 === side3) {
-    return "equilateralTriangle"
-  }
-
-  if (side1 === side2 || side2 === side3 || side3 === side1) {
-    return "isocelesTriangle"
-  }
-
-  if (side1 !== side2 && side2 !== side3) {
-    return "scaleneTriangle"
+    return ("notAtriangle")
+  } else if (side1 === side2 && side2 === side3) {
+    return ("equilateralTriangle")
+  } else if (side1 === side2 || side2 === side3 || side3 === side1) {
+    return ("isocelesTriangle")
+  } else {
+    return ("scaleneTriangle")
   }
 }
+
+$(document).ready(function() {
+  $("form#user-shape").submit(function(event) {
+    var side1 = parseInt($"input#side1").val();
+    var side2 = parseInt($"input#side2").val();
+    var side3 = parseInt($"input#side3").val();
+    var result = triangle(side1, side2, side3);
+
+      if (result === "notAtriangle") {
+        $(".triangle").text("not a triangle!");
+      } else if (result==="equilateralTriangle") {
+        $(".triangle").text("equilateral triangle");
+      }
+
+    event.preventDefault();
+  });
+});
