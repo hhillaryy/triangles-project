26ea87104ad929374fa8e3cd0da1f270b5ac23df
Hillary Hintzen <hillary07@gmail.com>
2016-01-19 16:25:36 -0800

    complete specs and scripts and begin html file

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ var triangle = function(side1, side2, side3) {
   if (side1+side2<side3 || side2+side3<side1 || side3+side1<side2) {
     return "not a triangle"
   }
+
+  if (side1 === side2 && side2 === side3) {
+    return "equilateralTriangle"
+  }
+
+  if (side1 === side2 || side2 === side3 || side3 === side1) {
+    return "isocelesTriangle"
+  }
+
+  if (side1 !== side2 && side2 !== side3) {
+    return "scaleneTriangle"
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,20 @@
 describe("triangle", function (){
-  it("is not a triangle if the sum of two sides are <= the third side", function(){
+  it("is not a triangle if the sum of two sides are < the third side", function(){
     expect(triangle(4,4,10)).to.equal("not a triangle");
   });
 
-  // it("is a triangle if all sides are equal", function(){
-  //   expect(triangle(13,13,13).to.equal("equilateral triangle"));
-  // });
+  it("is a triangle if all sides are equal", function(){
+    expect(triangle(13,13,13)).to.equal("equilateralTriangle");
+  });
+
+  it("is a triangle if two sides are equal", function(){
+    expect(triangle(13,13,15)).to.equal("isocelesTriangle");
+  });
+
+  it("is a triangle if no sides are equal", function(){
+    expect(triangle(4,13,15)).to.equal("scaleneTriangle");
+  });
+
+
 
 });
